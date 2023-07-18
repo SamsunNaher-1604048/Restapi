5b1aab650f1e5558bb636f6f4f4cfb4a5295cc87
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2023-07-18 08:30:21 +0600

    show nessage

--- app/Http/Controllers/api/StudentController.php
+++ app/Http/Controllers/api/StudentController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function index(){
+       $student = Student::all();
+
+       if($student->count()>0){
+        return response()->json([
+          'status'=>200,
+          'student'=> $student
+        ],200);
+       }
+       else{
+        return response()->json([
+            'status'=>404,
+            'student'=>"student not found"
+          ],200);
+
+       }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('student',function(){
-     return "this is student api";
-});
+Route::get('student',[StudentController::class,'index']);
