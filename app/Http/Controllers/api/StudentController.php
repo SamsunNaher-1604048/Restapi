4dbdad6070dd63870c54d3798f9999187c0db892
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2023-07-19 08:52:01 +0600

    update

--- app/Http/Controllers/api/StudentController.php
+++ app/Http/Controllers/api/StudentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
@@ -24,5 +25,46 @@ class StudentController extends Controller
           ],200);
 
        }
+    }
+
+
+    public function store(Request $request){
+      $validator  = Validator::make($request->all(),[
+        'name'=>'required',
+        'course'=>'required',
+        'phone'=>'required',
+        'email'=>'required',
+      ]);
+
+      if($validator->fails()){
+        return response()->json([
+          'status'=>422,
+          'error'=>$validator->messages()
+        ],422);
+      }
+      
+      else{
+        $student = Student::create([
+          'name'=>$request->name,
+          'course'=>$request->course,
+          'phone'=>$request->phone,
+          'email'=>$request->email
+        ]);
+
+        if($student){
+           return response()->json([
+              'status'=>200,
+              'message'=>'insert successfull'
+           ],200);
+        }
+        else{
+          return response()->json([
+            'status'=>500,
+            'message'=>'sonething is wrong'
+         ],500);
+        }
+
+      }
+
     }
 }
